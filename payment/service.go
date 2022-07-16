adc6c82d299f740cdba3afe5c3cd725d57818c14
icat108 <ichacnakval@gmail.com>
2022-07-17 03:18:49 +0700

    first commit

---
+++ payment/service.go
@@ -0,0 +1,48 @@
+package payment
+
+import (
+	"bwastartup/user"
+	"strconv"
+
+	midtrans "github.com/veritrans/go-midtrans"
+)
+
+type service struct {
+}
+
+type Service interface {
+	GetPaymentURL(transaction Transaction, user user.User) (string, error)
+}
+
+func NewService() *service {
+	return &service{}
+}
+
+func (s *service) GetPaymentURL(transaction Transaction, user user.User) (string, error) {
+	midclient := midtrans.NewClient()
+	midclient.ServerKey = "SB-Mid-server-6j-9-A94O7vdBYLgSM1OQ8ob"
+	midclient.ClientKey = "SB-Mid-client-ZcyKhheF50-BnuWt"
+	midclient.APIEnvType = midtrans.Sandbox
+
+	snapGateway := midtrans.SnapGateway{
+		Client: midclient,
+	}
+
+	snapReq := &midtrans.SnapReq{
+		CustomerDetail: &midtrans.CustDetail{
+			Email: user.Email,
+			FName: user.Name,
+		},
+		TransactionDetails: midtrans.TransactionDetails{
+			OrderID:  strconv.Itoa(transaction.ID),
+			GrossAmt: int64(transaction.Amount),
+		},
+	}
+
+	snapTokenResp, err := snapGateway.GetToken(snapReq)
+	if err != nil {
+		return "", err
+	}
+
+	return snapTokenResp.RedirectURL, nil
+}
